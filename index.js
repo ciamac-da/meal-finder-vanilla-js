88dbd2a535438264b0e16ce25bb900402c1397bd
ciamac-da <ciamac.davoudi@gmail.com>
2021-07-24 14:19:00 +0200

    Verified by Marti4n v1.0.4 show single meal page

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const term = search.value;
 // Check for empty
 if(term.trim()) {
   let API = `https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`
-  console.log(API)
   fetch(API)
   .then(res => res.json())
   .then(data => {
@@ -46,5 +45,60 @@ if(term.trim()) {
 }
 
 
+// Fetch Meal by ID
+const getMealById = (mealID) => {
+const API_ID = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`
+fetch(API_ID)
+.then( res => res.json())
+.then( data => {
+ const meal = data.meals[0]
+ addMealToDOM(meal)
+})
+}
+
+// Add Meal to DOM
+const addMealToDOM = (meal) => {
+const ingredients = []
+for(let i = 1 ; i <= 20 ; i++ ) {
+if(meal[`strIngredient${i}`]) {
+  ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
+} else {
+break;
+}
+}
+single_mealEl.innerHTML = `
+<div class="single-meal">
+<h1>${meal.strMeal}</h1>
+<img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+<div class="single-meal-info">
+${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+</div>
+<div class="main">
+<p>${meal.strInstructions}</p>
+<h2>Ingredients</h2>
+<ul>
+${ingredients.map(ing => `<li>${ing}</li>`).join("")}
+</ul>
+</div>
+</div>
+`
+}
+
 // Event Listeners
 submit.addEventListener("submit", searchMeal)
+
+//
+mealsEl.addEventListener("click", e => {
+const mealInfo = e.path.find(item => {
+    if(item.classList) {
+      return item.classList.contains("meal-info")
+    } else {
+      return false;
+    }
+  })
+  if(mealInfo) {
+    const mealID = mealInfo.getAttribute("data-mealID")
+    getMealById(mealID)
+  }
+})
